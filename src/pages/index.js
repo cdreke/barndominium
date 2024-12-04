5b46cea8e0be5777c176a3c698789de9adb2639d
Christian Dreke <cd@dreke.net>
2024-12-04 11:13:30 -0800

    updated designer url

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,9 +49,7 @@ const IndexPage = () => {
   ];
 
   const goToDesigner = () => {
-    navigate(
-      'https://urldefense.com/v3/__https:/actbuildingsystems.kayako.com/article/1027-set-up-your-online-3d-designer__;!!NW880qfKSBjB!qZaWq01jy9lpkfmO50TBy3qKsCUSPXahzIs1hDc4-4gaVIpXYVhWXJLvtELY5eWQilY5aVHTQ9rBVYI0tNVg8qtAtOqXeg$',
-    );
+    navigate('https://steelbuildinghawaii.actbuildingsystems.com/');
   };
 
   return (
